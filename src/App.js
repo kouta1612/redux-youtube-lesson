be0e5660ba0e9c9cee8a06cf538afa3c67d78fe6
hagiwara-lvgs <kota.hagiwara@leverages.jp>
2021-12-09 12:34:59 +0900

    finish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement } from './actions'
 
 function App() {
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+  const dispatch = useDispatch()
+
   return (
     <div className="App">
-      Hello
+      Counter: {counter}
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement(10))}>-</button>
+
+      {isLogged ? <h3>I'm logged in!</h3> : ''}
     </div>
   );
 }
