1b54212af2118c8a1290c204aed30e47598fcb9a
kouta1612 <kouta1612world69@gmail.com>
2021-12-09 09:56:03 +0900

    create reducer

--- src/index.js
+++ src/index.js
@@ -4,40 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux'
+import allReducers from './reducers'
 
-// ACTION
-const increment = () => {
-  return {
-    type: 'INCREMENT'
-  }
-}
-
-const decrement = () => {
-  return {
-    type: 'DECREMENT'
-  }
-}
-
-// REDUCER
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-  }
-}
-
-// STORE
-let store = createStore(counter)
-
-// display state in console.log
-store.subscribe(() => console.log(store.getState()))
-
-// DISPATCH
-store.dispatch(increment())
-store.dispatch(decrement())
-store.dispatch(decrement())
+const store = createStore(allReducers)
 
 ReactDOM.render(
   <React.StrictMode>
